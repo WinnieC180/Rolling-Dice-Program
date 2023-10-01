a3ae002ba77dde8082514b2205f17e1ef05a366a
Winnie Chan <winniec18@nycstudents.net>
2023-10-01 18:44:17 -0400

    initial

---
+++ script.js
@@ -0,0 +1,19 @@
+const button = document.querySelector("button");
+
+button.addEventListener("click", rollDice);
+
+function randomNum() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+const one = document.querySelectorAll("header")[0];
+const two = document.querySelectorAll("header")[1];
+const all = document.querySelectorAll("header")[2];
+
+function rollDice() {
+    var num1 = randomNum();
+    var num2 = randomNum();
+    one.textContent = `Dice #1 value: ${num1}`;
+    two.textContent = `Dice #2 value: ${num2}`;
+    all.textContent = `Dice #3 value: ${num1 + num2}`;
+}
